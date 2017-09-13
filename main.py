413a8699453d2fe6c3eb1b302c9d6550505aacd9
mar0113 <salvinom@gilmour.org>
2017-09-13 14:33:35 -0400

    hello robot

---
+++ main.py
@@ -0,0 +1,23 @@
+from gopigo import *
+import time
+
+##### MY CLASS
+
+class Piggy(object):
+
+    def __init__(self):
+
+        print("Hello, I'm your robot")
+        self.dance()
+
+    def dance(self):
+        fwd()
+        time.sleep(1.5)
+        right_rot()
+        time.sleep(.5)
+        stop()
+
+
+##### MY APP
+
+marisa = Piggy()
